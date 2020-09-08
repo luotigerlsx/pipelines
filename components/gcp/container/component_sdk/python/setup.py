14dcb37f0e6f977fb230a1e34b7bda6ed77a4b95
Shixin Luo <luoshixin@google.com>
2020-09-08 12:28:53 +0800

    modify http agent patch with new google api sdk version

--- components/gcp/container/component_sdk/python/setup.py
+++ components/gcp/container/component_sdk/python/setup.py
@@ -24,7 +24,7 @@ setup(
     author='google',
     install_requires=[
         'kubernetes >= 8.0.1', 'urllib3>=1.15,<1.25', 'fire == 0.1.3',
-        'google-api-python-client == 1.7.8', 'google-cloud-storage == 1.14.0',
+        'google-api-python-client == 1.8.4', 'google-cloud-storage == 1.14.0',
         'google-cloud-bigquery == 1.9.0'
     ],
     packages=[
